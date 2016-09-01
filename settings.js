236efba80cd2bc6b3381cf4957e5566b9e7f39a9
xudsong <xudsong@163.com>
2016-09-01 10:50:24 +0800

    tiaojiao

---
+++ settings.js
@@ -0,0 +1,16 @@
+﻿(function() {
+    var settings;
+
+    settings = {
+        db: {
+            host: 'localhost',     //本地数据库
+            port: '3306',
+            user: 'root',          //数据库用户名
+            password: 'xds18720061484',          //数据库密码
+            database: 'microblog'  //数据库名称
+        }
+    };
+
+    module.exports = settings;
+
+}).call(this);
